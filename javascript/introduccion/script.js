396c6804d1118c8fce69a84cfce84c742590dd8d
AnonDEV23 <luisayvalballesteros@icloud.com>
2023-06-06 18:39:48 -0500

    so many changes lol

--- javascript/introduccion/script.js
+++ javascript/introduccion/script.js
@@ -1,47 +1,94 @@
-console.log('Calendario 1.0')
-
-const daysOfWeek = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
-const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-
-const currentDate = new Date();
-const currentMonth = currentDate.getMonth();
-const currentYear = currentDate.getFullYear();
-
-const firstDayOfMonth = new Date(currentYear, currentMonth, 1);
-
-const numberOfDaysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
-
-const calendarTable = document.createElement('table');
-
-const headerRow = calendarTable.insertRow();
-const headerCell = headerRow.insertCell();
-headerCell.colSpan = 7;
-headerCell.textContent = months[currentMonth] + ' ' + currentYear;
-headerCell.style.textAlign = 'center';
-
-const daysOfWeekRow = calendarTable.insertRow();
-for (let i = 0; i < 7; i++) {
-  const dayCell = daysOfWeekRow.insertCell();
-  dayCell.textContent = daysOfWeek[i];
-  dayCell.style.textAlign = 'center';
-}
-
-let date = 1;
-for (let i = 0; i < 6; i++) {
-  const weekRow = calendarTable.insertRow();
-  for (let j = 0; j < 7; j++) {
-    const dayCell = weekRow.insertCell();
-    if (i === 0 && j < firstDayOfMonth.getDay()) {
-      dayCell.textContent = '';
-    } else if (date > numberOfDaysInMonth) {
-      dayCell.textContent = '';
-    } else {
-      dayCell.textContent = date;
-      dayCell.style.textAlign = 'center';
-      date++;
-    }
+// Age Verification Form
+const ageForm = document.getElementById("ageForm");
+ageForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  
+  const birthYearInput = event.target.querySelector("#birthYear");
+  const birthYear = Number(birthYearInput.value);
+  
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - birthYear;
+  
+  if (age >= 18) {
+    alert("You are old enough to continue");
+    
+    const beerForm = document.getElementById("beerFrom");
+    beerForm.classList.remove("hidden");
+  } else {
+    alert("You are not old enough to continue");
+    birthYearInput.focus();
   }
-}
+});
 
-const calendarContainer = document.getElementById('calendar-container');
-calendarContainer.appendChild(calendarTable);
+// Beer Form
+const beerForm = document.getElementById("beerFrom");
+beerForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const beerChooseInput = event.target.querySelector("#beerChoose");
+  const beerChoose = beerChooseInput.value.toLowerCase().trim();
+
+  if (beerChoose === "yes" || beerChoose === "no") {
+    alert("Your answer has been recorded");
+
+    const drunkedForm = document.getElementById("drunkedForm");
+    drunkedForm.classList.remove("hidden");
+  } else {
+    alert("Please enter a valid response (Yes or No)");
+    beerChooseInput.focus();
+  }
+});
+
+// Drunk Form
+const drunkedForm = document.getElementById("drunkedForm");
+drunkedForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const drunkChooseInput = event.target.querySelector("#drunkChoose");
+  const drunkChoose = drunkChooseInput.value.toLowerCase().trim();
+
+  if (drunkChoose === "yes" || drunkChoose === "no" || drunkChoose === "") {
+    alert("Your answer has been recorded");
+
+    const peopleForm = document.getElementById("peopleForm");
+    peopleForm.classList.remove("hidden");
+  } else {
+    alert("Please enter a valid response (Yes or No)");
+    drunkChooseInput.focus();
+  }
+});
+
+// People Form
+const peopleForm = document.getElementById("peopleForm");
+peopleForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const peopleNumberInput = event.target.querySelector("#peopleNumber");
+  const peopleNumber = Number(peopleNumberInput.value);
+
+  if (peopleNumber >= 0) {
+    alert("Your answer has been recorded");
+
+    const sleepForm = document.getElementById("sleepForm");
+    sleepForm.classList.remove("hidden");
+  } else {
+    alert("Please enter a valid number");
+    peopleNumberInput.focus();
+  }
+});
+
+// Sleep Form
+const sleepForm = document.getElementById("sleepForm");
+sleepForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const sleepHoursInput = event.target.querySelector("#sleepHours");
+  const sleepHours = Number(sleepHoursInput.value);
+
+  if (sleepHours >= 0 && sleepHours <= 24) {
+    alert("Your form has been successfully completed");
+  } else {
+    alert("Please enter a valid time in hours (0-24)");
+    sleepHoursInput.focus();
+  }
+});
