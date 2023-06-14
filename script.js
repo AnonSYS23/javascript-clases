5149718dfbbe1cf09ced33896d2cce5139e2ea8e
AnonDEV23 <luisayvalballesteros@icloud.com>
2023-06-14 17:58:00 -0500

    lol

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
-// Este código se ejecutará cuando el DOM haya sido completamente cargado
-document.addEventListener('DOMContentLoaded', function() {
-    // Obtiene la referencia a los elementos del menú de navegación
-    var menuItems = document.querySelectorAll('nav ul li');
-  
-    // Agrega el evento 'click' a cada elemento del menú
-    menuItems.forEach(function(item) {
-      item.addEventListener('click', function(event) {
-        // Evita el comportamiento predeterminado del enlace
-        event.preventDefault();
-  
-        // Obtiene el valor del atributo 'href' del enlace
-        var target = this.querySelector('a').getAttribute('href');
-  
-        // Desplaza suavemente la ventana hacia el objetivo utilizando el método 'scrollIntoView'
-        document.querySelector(target).scrollIntoView({
-          behavior: 'smooth'
-        });
-      });
-    });
-  });
-  
+function appendValue(value) {
+  document.getElementById('result').value += value;
+}
+
+function clearResult() {
+  document.getElementById('result').value = '';
+}
+
+function deleteLast() {
+  var result = document.getElementById('result').value;
+  document.getElementById('result').value = result.slice(0, -1);
+}
+
+function calculate() {
+  var result = document.getElementById('result').value;
+  var answer = eval(result);
+  document.getElementById('result').value = answer;
+}
