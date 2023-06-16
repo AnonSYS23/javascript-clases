4955a367435a6fc5ea7225fa42725974aa470877
AnonDEV23 <luisayvalballesteros@icloud.com>
2023-06-16 17:31:22 -0500

    so many changes

---
+++ example/login.js
@@ -0,0 +1,7 @@
+/* JavaScript for the login form */
+$(document).ready(function() {
+  // Show the password input when the user clicks on the eye icon
+  $("#show-password").click(function() {
+    $("#password").toggle();
+  });
+});
